f1124ebed7d7d90afc58747e86766f79982ad3ab
kimmingyu0 <minku4820@gmail.com>
2023-10-15 20:10:14 +0900

    ch5-1 config server git에서 환경 변수 가져오기

---
+++ licensing-service/src/test/java/com/optimagrowth/license/MsaCh3ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.optimagrowth.license;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaCh3ApplicationTests {
+
+    @Disabled
+    void contextLoads() {
+    }
+
+}
